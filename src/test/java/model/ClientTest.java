6cb29b737083063ce5ff4c2582cef7563aafab82
Jean D <jean.deglaire@gmail.com>
2017-08-16 12:05:28 +0200

    ajouté une classe de test et supprimé le dummy

---
+++ src/test/java/model/ClientTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest {
+
+	@Test
+	public void testPersonne() {
+		fail("Not yet implemented");
+	}
+
+}
