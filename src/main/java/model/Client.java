ab6b581a9eb9e3fa0137f60d1346aa81a36fefa5
Jean D <jean.deglaire@gmail.com>
2017-08-16 11:47:24 +0200

    ajout des images balsamiq et début de mise en place des beans metiers en JPA

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Client {
+
+public class Client extends Personne{
+	private String email;
+	private String adresse;
+	private String codePostal;
+	private String ville;
+	private String telephone;
+	private CompteCourant compteCourant;
+	private CompteEpargne compteEpargne;
+	private boolean isEntreprise; 
 
 }
